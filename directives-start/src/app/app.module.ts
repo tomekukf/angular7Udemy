35ddec1719aecd8137de3f618d578a00e89cf83c
tomekukf <tomek.ukf@gmail.com>
2019-05-14 20:19:55 +0200

    1st push

---
+++ directives-start/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { ChangeColorDirective } from './change-color.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChangeColorDirective
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
