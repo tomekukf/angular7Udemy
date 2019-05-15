4e3811f7516722d0a62d3a51068d1dd52f7485c0
tomekukf <tomek.ukf@gmail.com>
2019-05-15 22:39:01 +0200

    services update

--- Services/src/app/new-account/new-account.component.ts
+++ Services/src/app/new-account/new-account.component.ts
@@ -1,18 +1,30 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import {LoggingService} from '../services/loggin.service';
+import {AccountService} from '../services/accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
-    console.log('A server status changed, new status: ' + accountStatus);
+
+  constructor(private loggingService: LoggingService
+  , private accountService: AccountService) {
+
+  }
+
+    onCreateAccount(accountName: string, accountStatus: string) {
+    this.accountService.addAccount(accountName, accountStatus );
+    // this.accountAdded.emit({
+    //   name: accountName,
+    //   status: accountStatus
+    // });
+    this.loggingService.logStatusChanged(accountStatus);
+
+    // console.log('A server status changed, new status: ' + accountStatus);
   }
 }
---
+++ Services/src/app/services/accounts.service.ts
@@ -0,0 +1,32 @@
+import {Injectable} from '@angular/core';
+import {LoggingService} from './loggin.service';
+
+@Injectable()
+export class AccountService{
+
+  constructor(private loggingServide: LoggingService){
+
+  }
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'active'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+
+  addAccount(name: string, status: string) {
+    this.accounts.push({name: name, status: status});
+  }
+
+  updateStatus(id: number, status: string) {
+    this.accounts[id].status = status;
+  }
+}
